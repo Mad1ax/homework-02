ba45b2d64f5fe8a2cef4c236abd5727372c596f4
Mad1ax <62326559+Mad1ax@users.noreply.github.com>
2021-08-30 22:43:44 +0700

    Delete first-react-app directory

--- first-react-app/src/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-import ReactDom from 'react-dom'
-
-const element = <h1>Hello world, меня зовут: Максим Баранов</h1>
-ReactDom.render(element, document.getElementById('root'))
